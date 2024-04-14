88218689ceaa3eaa500cd15334ae8481f382dba4
tillyJay <tilly.jay.campbell@gmail.com>
2024-04-14 14:41:36 -0300

    - created studentPage

--- functions.php
+++ functions.php
@@ -111,6 +111,16 @@ function itjobfair_enqueue_industrypage_styles() {
 }
 add_action('wp_enqueue_scripts', 'itjobfair_enqueue_industrypage_styles');
 
+//student page styles
+function itjobfair_enqueue_industrypage_styles() {
+    if (is_page('students')) {
+        wp_enqueue_style('studentPage', get_template_directory_uri() . '/assets/css/studentPage.css', array(), '1.0.0', 'all');
+    }
+}
+add_action('wp_enqueue_scripts', 'itjobfair_enqueue_studentpage_styles');
+
+
+
 
 //enqueue custom JavaScript
 function itjobfair_enqueue_scripts() {
